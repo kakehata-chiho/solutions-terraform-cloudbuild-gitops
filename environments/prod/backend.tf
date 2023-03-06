310d3051f7bfa4d68fb3acaabfeb473d5daec5bd
kakehata.chiho <kakehata.chiho@j.cloud-ace.jp>
2023-03-06 06:13:45 +0000

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "ca-srebpstudy-kakehata-1-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="ca-srebpstudy-kakehata-1"
